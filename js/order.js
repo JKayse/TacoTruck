a92c2f0b59d7acd66c4bf68ec66c16c9b72fa39c
JKayse <jkayse@smu.edu>
2014-03-03 17:35:51 -0600

    You can now add up the price of the ingredients for each taco.

--- js/order.js
+++ js/order.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     $(".fillingOption").click(addFillingSelector);
     $( "#sauce input" ).each(addheatImage);
     $("#menuButtons button").click(addOrder);
+    $("#orderMenu div").click(addCurrentCost);
 
 
 });
@@ -37,8 +38,69 @@ function addheatImage(event){
 
 function addOrder(event){
     var tacoName = $("#filling .selectedImage h4").text();
-    $("#addedOrders").append("<button type='button'>X</button><button type='button'>"+ tacoName + " Taco</button><input type='number' class ='quantity' name='quantity' placeholder='Quantity' min='1'><br>");
+    var tacoPrice = parseFloat($("#tacoPrice").attr("tacoPrice")).toFixed(2);
+    $("#addedOrders").append("<button type='button'>X</button><button type='button'>"+ tacoName + " Taco</button><input type='number' class ='quantity' name='quantity' value='1' min='1'><span class='tacoPrice'>$"+tacoPrice+"</span><br>");
 }
 
+
+function addCurrentCost()
+{
+    var fillingPrice = $("#filling .selectedImage img").attr("price");
+    fillingPrice = parseFloat(fillingPrice);
+
+    var tortillaPrice = $("#tortilla .selectedImage img").attr("price");
+    tortillaPrice = parseFloat(tortillaPrice);
+
+    var cheese = $("#cheese input[type='radio']:checked");
+    var cheesePrice = 0;
+    if(cheese.size() !== 0){
+        cheesePrice = cheese.attr("price");
+        cheesePrice = parseFloat(cheesePrice);
+    }
+
+
+    var rice = $("#rice input[type='radio']:checked");
+    var ricePrice = 0;
+    if(rice.size() !== 0){
+        ricePrice = rice.attr("price");
+        ricePrice = parseFloat(ricePrice);
+    }
+
+
+    var beansPrice = $("#beans input[type='radio']:checked").attr("price");
+    beansPrice = parseFloat(beansPrice);
+
+    var beans = $("#beans input[type='radio']:checked");
+    var beansPrice = 0;
+    if(beans.size() !== 0){
+        beansPrice = beans.attr("price");
+        beansPrice = parseFloat(beansPrice);
+    }
+
+
+    var extras = $("#extras input[type='checkbox']:checked");
+    var totalExtrasPrice = 0;
+    var extrasPrice = 0;
+    for(var i = 0; i < extras.size(); i++){
+        extrasPrice = $(extras.get(i)).attr("price");
+        extrasPrice = parseFloat(extrasPrice);
+        totalExtrasPrice = extrasPrice + totalExtrasPrice;
+    }
+
+    totalExtrasPrice = totalExtrasPrice*100;
+    totalExtrasPrice = Math.round(totalExtrasPrice);
+    totalExtrasPrice = totalExtrasPrice/100;
+
+
+    totalPrice = tortillaPrice + fillingPrice + cheesePrice + ricePrice + beansPrice + totalExtrasPrice;
+    totalPrice = totalPrice*100;
+    totalPrice = Math.round(totalPrice);
+    totalPrice = totalPrice/100;
+    totalPrice =totalPrice.toFixed(2);
+    $("#tacoPrice").html("Current Price for each taco: $" + totalPrice);
+    $("#tacoPrice").attr("tacoPrice",totalPrice);
+
+
+}
 
 
