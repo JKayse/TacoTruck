33a144f6b57a53053908b9bb508cf2504cf18ab2
JKayse <jkayse@smu.edu>
2014-03-03 00:16:37 -0600

    Added the options for the menu and the pictures.

--- js/order.js
+++ js/order.js
@@ -7,4 +7,20 @@ $(function() {
 
 $(document).ready(function() {
     $("header").load("Header.html");
+    $(".tortillaOption").click(addTortillaSelector);
+    $(".fillingOption").click(addFillingSelector);
+
 });
+
+function addTortillaSelector(event){
+    $(".tortillaOption").removeClass("selectedImage");
+    $(this).addClass("selectedImage");
+}
+
+function addFillingSelector(event){
+    $(".fillingOption").removeClass("selectedImage");
+    $(this).addClass("selectedImage");
+}
+
+
+
