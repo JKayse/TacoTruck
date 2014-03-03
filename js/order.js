1fd8695dcc14497cbee2815cb92e633656119a38
JKayse <jkayse@smu.edu>
2014-03-03 14:46:44 -0600

    Added the heat rating as well as adding a taco to the menu.

--- js/order.js
+++ js/order.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
     $("header").load("Header.html");
     $(".tortillaOption").click(addTortillaSelector);
     $(".fillingOption").click(addFillingSelector);
+    $( "#sauce input" ).each(addheatImage);
+    $("#menuButtons button").click(addOrder);
+
 
 });
 
@@ -20,6 +23,21 @@ function addTortillaSelector(event){
 function addFillingSelector(event){
     $(".fillingOption").removeClass("selectedImage");
     $(this).addClass("selectedImage");
+}
+
+
+function addheatImage(event){
+    var element = $(this);
+    var numImg = element.attr("heatrating");
+    var name = element.attr("value");
+    for(var i = 0; i < numImg; i++){
+       $(this).next().append("<img src='img/heat.png'/>");
+    }
+}
+
+function addOrder(event){
+    var tacoName = $("#filling .selectedImage h4").text();
+    $("#addedOrders").append("<button type='button'>X</button><button type='button'>"+ tacoName + " Taco</button><input type='number' class ='quantity' name='quantity' placeholder='Quantity' min='1'><br>");
 }
 
 
