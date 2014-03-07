ad2869b9c54c8d221fe30f74b163065a898abcec
Maryssa <mecrews@smu.edu>
2014-03-07 10:58:56 -0600

    added linking functions to JS

--- js/index.js
+++ js/index.js
@@ -2,4 +2,33 @@
 
 $(document).ready(function() {
     $("header").load("Header.html");
+    $(document).on('click', "header img", goToHomePage);
+    $(document).on('click', "header h1", goToHomePage);
+    $(document).on('click', "#homepage", goToHomePage);
+    $(document).on('click', "#guestOrder", goToOrderPage);
+    $(document).on('click', "#locations", goToMap);
+    $(document).on('click', "#signUp", signUp);
+    $(document).on('click', "header #signIn button", signIn);
+    $(document).on('click', "header #signedIn a", signOut);
+}
+
+function goToHomePage(){
+    window.location = "index.html";
+}
+
+function goToOrderPage(){
+    window.location = "orderpage.html";
+}
+
+function goToMap(){
+    window.location = "map.html";
+}
+
+function signUp{
+}
+
+function signIn{
+}
+
+function signOut{
 }
